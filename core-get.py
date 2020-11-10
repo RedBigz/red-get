e42bc02ce39927e1ab12db66461a36cd9605e02a
Red <72632024+RedBigz@users.noreply.github.com>
2020-11-10 18:44:24 +1000

    Create core-get.py

---
+++ core-get.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print("red-get is still under development. When it has been finished, reinstall red-get.")
+
+#import os
+#import sys
+
+#if len(argv) == 1:
+#  print("Syntax: red-get")
